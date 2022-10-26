42c1c7de959a11feed6153c12871e6290d48662a
D3V0 <davidamgad2@icloud.com>
2022-10-26 07:29:42 +0200

    first migrations for 1st phase

---
+++ Course/models.py
@@ -0,0 +1,50 @@
+from random import choices
+from wsgiref.validate import validator
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Create your models here.
+
+
+class Course():
+    """Hnadling courses model"""
+    name = models.CharField(max_length=300)
+    #reviews = models.ManyToManyField(Review)
+    price = models.IntegerField()
+    includes = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='Courses images')
+    description = models.TextField()
+    overview = models.TextField()
+    #instructor = models.ManyToManyField(Instructor)
+    language = models.CharField(choices=[
+        'English', 'Arabic', 'German', 'French'
+    ])
+    rate = models.IntegerField(
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ]
+    )
+    date = models.DateTimeField()
+
+
+class Instructor():
+    """Handling instructor model"""
+    name = models.CharField(max_length=300)
+    description=models.TextField()
+    number=models.IntegerField()
+    courses=models.TextField() #hna al mfrod a5od al courses ali ll ragl dh
+
+
+class Review():
+    """Handling Reviews model"""
+    name=models.CharField(max_length=300) #hna al mfrod a5od asm al user ali hy3ml al review
+    comment=models.TextField()
+    rate = models.IntegerField(
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ]
+    )
---
+++ README.md
@@ -0,0 +1,3 @@
+# Course Website API
+
+This website is for courses, this project will include the APIs using the rest framework.
